f303c5e54446002bac3fe0e072bc5c85b84a010f
DanilVelichko <velichko.danil@gmail.com>
2023-02-04 19:33:08 +0100

    logOut

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
  import { Layout } from './Layout';
 import { LoginPage } from 'pages/LoginPage';
 import { Routes, Route } from 'react-router-dom';
+import { DashboardPage } from 'pages/DashboardPAge';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="login" element={<LoginPage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
       </Route>
     </Routes>
   );
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,18 @@
 import { useDispatch } from "react-redux";
 import { logIn } from "redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 
 export const LoginForm = () => {
-
+    const navigate = useNavigate();
     const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
     const form = e.currentTarget;
     dispatch(logIn(form.elements.login.value));
-    form.reset();
+      form.reset();
+      navigate('/dashboard', {replace: true})
   };
 
   return (
