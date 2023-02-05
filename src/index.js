d23e2fe593c0c17f067d05f369f75ec6e8ebf4c9
DanilVelichko <velichko.danil@gmail.com>
2023-02-05 13:27:12 +0100

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
-import { store } from "redux/store";
+import { store } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
     <Provider store={store}>
       <BrowserRouter basename="Redux-trainy">
-        <App />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>{' '}
+      </BrowserRouter>
     </Provider>
-
   </React.StrictMode>
 );
